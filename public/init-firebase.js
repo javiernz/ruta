8df15cb7bda8ce7feedb4d54a681212201544f4f
Javier Muñoz <48762634+javiernz@users.noreply.github.com>
2022-12-21 09:52:17 +0000

    Added edit proveedor full section

--- public/init-firebase.js
+++ public/init-firebase.js
@@ -5,107 +5,65 @@ const nombre = document.getElementById('nombre');
 const zona = document.getElementById('zona');
 const horario = document.getElementById('horario');
 const notas = document.getElementById('notas');
-
-const startRsvpButton = document.getElementById('startRsvp');
-
 const form = document.getElementById('formBuscar');
-const input = document.getElementById('message');
-const guestbook = document.getElementById('guestbook');
-const numberAttending = document.getElementById('number-attending');
-const rsvpYes = document.getElementById('rsvp-yes');
-const rsvpNo = document.getElementById('rsvp-no');
 
-let rsvpListener = null;
-let guestbookListener = null;
+// Current provider
 let currentProveedor = null;
   
+// Inizialize firebase app
 firebase.initializeApp(firebaseConfig);
 
-// Option 2: Access Firebase services using shorthand notation
-auth = firebase.auth();
+// Inizialize firestore database
 db = firebase.firestore();
-
-// FirebaseUI config
-const uiConfig = {
-  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-  signInOptions: [{
-    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    requireDisplayName: false,
-    disableSignUp: {
-      status: true,
-      adminEmail: '',
-      helpLink: 'https://www.example.com/trouble_signing_in',
-    },
-  }],
-  callbacks: {
-    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-    // Handle sign-in.
-    // Return false to avoid redirect.
-    return false;
-    },
-  },
-};
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(auth);
-
-// Listen to login button clicks
-// Called when the user clicks the RSVP button
-startRsvpButton.addEventListener('click', () => {
-  if (auth.currentUser) {
-    // User is signed in; allows user to sign out
-    auth.signOut();
-  }
-  else {
-    // No user is signed in; allows user to sign in
-    ui.start('#firebaseui-auth-container', uiConfig);
-  }
-});
-
-/*
-// Listen to the current Auth state
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    startRsvpButton.textContent = 'Cerrar Sesión';
-    form.classList.remove('d-none');
-    startRsvpButton.classList.add('d-none');
-  } 
-  else {
-    startRsvpButton.textContent = 'Iniciar Sesión';
-    form.classList.add('d-none');
-    startRsvpButton.classList.remove('d-none');
-    ficha.classList.add('d-none');
-  }
-});
-*/
 
 // Listen to the form submission
 form.addEventListener('submit', async (e) => {
   // Prevent the default form redirect
   e.preventDefault();
 
+  // Set current Proveedor from input field
   currentProveedor = ccip.value;
 
+  // Set database ref from current Proveedor variable
   var docRef = db.collection('proveedores').doc(currentProveedor);
 
+  // If document exists, return data to html table, if not, return erro
   docRef.get().then((doc) => {
       if (doc.exists) {
-          nombre.innerHTML = '(' + currentProveedor + ') ' + doc.data().nombre;
-          zona.innerHTML = doc.data().zona;
-          horario.innerHTML = doc.data().horario;
-          notas.innerHTML = doc.data().notas;
-          ficha.classList.remove('d-none');
-      } else {
+        if (ficha) {
+            nombre.innerHTML = '(' + currentProveedor + ') ' + doc.data().nombre;
+            zona.innerHTML = doc.data().zona;
+            horario.innerHTML = doc.data().horario;
+            notas.innerHTML = doc.data().notas;
+            ficha.classList.remove('d-none');
+        }
+        else if (fichaeditar) {
+            nombre.value = doc.data().nombre;
+            zona.value = doc.data().zona;
+            horario.value = doc.data().horario;
+            notas.value = doc.data().notas;
+            fichaeditar.classList.remove('d-none');
+        }
+      }
+      else {
           console.log('No such document!');
-          ficha.classList.add('d-none');
+          if (ficha) {
+            ficha.classList.add('d-none');
+          }
+          else if (fichaeditar) {
+            fichaeditar.classList.add('d-none');
+          }
       }
   }).catch((error) => {
       console.log('Error getting document:', error);
       ficha.classList.add('d-none');
   });
 
-  // Empty input
+  // Empty input field
   ccip.value = '';
+  if (correctamente) {
+    correctamente.classList.add('d-none');
+  }
 
   // Return false to avoid redirect
   return false;
